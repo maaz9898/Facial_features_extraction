527d9f517993526677e6c46c8fffd5f077125df7
Mahmoud Mostafa <msabra388@gmail.com>
2022-03-05 12:49:58 +0200

    Added skin-tone extraction feature

--- driver.py
+++ driver.py
@@ -2,16 +2,19 @@ import argparse
 import cv2
 import json
 import os
+from SkinToneFinder import findSkinTone
 from FeatureExtractor import FeatureExtractor
+
 
 """ This is the driver script that uses the FeatureExtractor class"""
 def main():
     # Define the argument parser along with the arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("-input", help="Input image or directory")
-    parser.add_argument("-face_orient", default=True, help="True for adding face orientation")
-    parser.add_argument("-mouth", default=True, help="True for adding mouth extraction")
-    parser.add_argument("-face_segment", default=True, help="True for adding face segmentation")
+    parser.add_argument("--input", help="Input image or directory")
+    parser.add_argument("--face_orient", default=True, help="True for adding face orientation")
+    parser.add_argument("--extract_mouth", default=True, help="True for adding mouth extraction")
+    parser.add_argument("--segment_face", default=True, help="True for adding face segmentation")
+    parser.add_argument("--extract_skintone", default=True, help="True for adding skin-tone extraction")
     args = vars(parser.parse_args())
 
     # Apply feature extraction on a whole directory
@@ -21,7 +24,7 @@ def main():
             
             FExtractor = FeatureExtractor(image)  # Create a FeatureExtractor object
 
-            features = FExtractor.extractFeatures(args['face_orient'], args['mouth'])  # Call the extractFeatures() method
+            features = FExtractor.extractFeatures(args['face_orient'], args['extract_mouth'])  # Call the extractFeatures() method
 
             writePath = args['input'] + imgPath.split('.')[0]  # Specify the path to write to
             
@@ -31,20 +34,24 @@ def main():
             json.dump(features, out_file, indent=4)
             out_file.close()
 
-            faces = FExtractor.segmentFace(imgPath)
+            if args['segment_face']:
+                faces = FExtractor.segmentFace(imgPath)
 
-            counter = 0
-            # Iterate over the cropped faces images
-            for face in faces:
-                cv2.imwrite(writePath + '_face_' + str(counter) + '.png', face)
-                counter +=1
+                counter = 0
+                # Iterate over the cropped faces images
+                for face in faces:
+                    cv2.imwrite(writePath + '_face_' + str(counter) + '.png', face)
+                    counter +=1
+
+            if args['extract_skintone']:
+                findSkinTone(args['input'])
     
     # Apply feature extraction on a single image
     else:
         image = cv2.imread(args['input'])  # Read the input image
         FExtractor = FeatureExtractor(image)  # Create a FeatureExtractor object
 
-        features = FExtractor.extractFeatures(args['face_orient'], args['mouth'])  # Call the extractFeatures() method
+        features = FExtractor.extractFeatures(args['face_orient'], args['extract_mouth'])  # Call the extractFeatures() method
 
         writePath = args['input'].split('.')[0]  # Specify the path to write .json to
         out_file = open(writePath + '.json', "w")  # Create .json file with writing privilege
@@ -55,12 +62,17 @@ def main():
 
         faces = FExtractor.segmentFace(args['input'])
 
-        counter = 0
-        # Iterate over the cropped faces images
-        for face in faces:
-            # saving final transparent image
-            cv2.imwrite(writePath + '_face_' + str(counter) + '.png', face)
-            counter +=1
+        if args['segment_face']:
+            counter = 0
+            # Iterate over the cropped faces images
+            for face in faces:
+                # saving final transparent image
+                cv2.imwrite(writePath + '_face_' + str(counter) + '.png', face)
+                counter +=1
+
+        if args['extract_skintone']:
+            for face in faces:
+                findSkinTone(args['input'])
 
 if __name__ == '__main__':
     main()
