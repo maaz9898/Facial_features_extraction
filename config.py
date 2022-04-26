a6352416a6c5f616ea2ef9fd9fc1f40f7ab88955
Syed Abdullah Muzaffar <56280132+samuzaffar99@users.noreply.github.com>
2022-04-26 20:53:28 +0500

    remove https to fix self urls

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
-BASE_URL = 'https://staging.taptapstories.dk'
+BASE_URL = 'http://127.0.0.1:5001'
 
 # Output folder where images are saved
 WRITE_PATH = './static/output/'
 
 # Image endpoint URL
-IMG_PATH = f'{BASE_URL}/mask/image?img=/static/output/'
+IMG_PATH = f'{BASE_URL}/static/output/'
 
 # Features endpoint URL
 FEATURES_PATH = f'{BASE_URL}/mask/features?image={BASE_URL}/static/output/'
+
+# Minimum Confidence threshold for face detection
+MIN_CONF = 0.5
