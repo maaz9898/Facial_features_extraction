eff2af202be024e08275bfddffcae5d771f8eded
Syed Abdullah Muzaffar <56280132+samuzaffar99@users.noreply.github.com>
2022-04-26 05:00:59 +0500

    update endpoints and move to config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+BASE_URL = 'https://staging.taptapstories.dk'
+
+# Output folder where images are saved
+WRITE_PATH = './static/output/'
+
+# Image endpoint URL
+IMG_PATH = f'{BASE_URL}/mask/image?img=/static/output/'
+
+# Features endpoint URL
+FEATURES_PATH = f'{BASE_URL}/mask/features?image={BASE_URL}/static/output/'
